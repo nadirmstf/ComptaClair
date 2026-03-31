8c3c9efad8fbf3c01edd2c4ab492b41668074585
Jalel <jalel.elhamdi92@gmail.com>
2026-03-31 16:16:25 +0200

    Début BDD + Dashboad avec Custom Tkinter

---
+++ db/database.py
@@ -0,0 +1,45 @@
+import sqlite3
+import hashlib
+
+conn = sqlite3.connect("tables.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS depenses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    montant REAL NOT NULL,
+    categorie TEXT,
+    description TEXT,
+    date TEXT,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+)
+""")
+
+conn.commit()
+
+
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def creer_user(username, password):
+    pwd_hash = hash_password(password)
+    cursor.execute(
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        (username, pwd_hash)
+    )
+    conn.commit()
+    print(f"Utilisateur {username} créé avec succès !")
+
+creer_user("noir","manger778")
+
+conn.close()
--- ui/app.py
+++ ui/app.py
@@ -1,11 +1,51 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-import sys
+from tkinter import *
+from tkinter import ttk
+import customtkinter as ctk
+import ctypes
 
-app = QApplication(sys.argv) 
- 
-fenetre = QMainWindow()        
-fenetre.setWindowTitle("ComptaClair - Suivez vos dépenses")
-fenetre.setFixedSize(1300, 600) 
-fenetre.show()                 
+# La fenêtre (taille, icon, titre)
+fenetre = ctk.CTk()
+fenetre.title("Compta Clair")
+fenetre.iconbitmap("assets\img\icon.ico")
+fenetre.geometry("600x400")
 
-sys.exit(app.exec())           
+
+# Charger la police Montserrat
+ctypes.windll.gdi32.AddFontResourceW("assets/fonts/Montserrat/Montserrat-Light.ttf")
+
+#Side Bar
+sidebar = ctk.CTkFrame(fenetre, width=200, corner_radius=0, fg_color="#2b2b2b")
+sidebar.pack(side="left", fill="y")
+sidebar.pack_propagate(False)
+
+
+btn_dashboard = ctk.CTkButton(sidebar, text="🔰​ Tableau de bord", fg_color="gray30", hover_color="gray")
+btn_dashboard.pack()
+
+btn_dashboard = ctk.CTkButton(sidebar, text="​💲​ Dépenses", fg_color="gray30", hover_color="gray")
+btn_dashboard.pack()
+
+btn_dashboard = ctk.CTkButton(sidebar, text="📊​ Graphiques", fg_color="gray30", hover_color="gray")
+btn_dashboard.pack()
+
+
+contenu = ctk.CTkFrame(fenetre, corner_radius=0, fg_color="#1f1f1f")
+contenu.pack(side="right", fill="both", expand=True)
+
+
+
+# tabs = ctk.CTkTabview(fenetre, anchor="w")
+# tabs.pack(fill="both", expand=True, padx=20, pady=20)
+
+# # Ajouter les onglets
+# tabs.add("Tableau de Bord")
+# tabs.add("Dépenses")
+# tabs.add("Graphiques")
+
+
+
+
+fenetre.mainloop()
+
+
+
