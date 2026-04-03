1ba1af4de79e31882b5bdf42d84632b355eaa62c
Jalel <jalel.elhamdi92@gmail.com>
2026-04-03 10:00:29 +0200

    Logo, sidebar, boutons ajoutés

--- ui/app.py
+++ ui/app.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import customtkinter as ctk
+from PIL import Image
 import ctypes
 
 # La fenêtre (taille, icon, titre)
@@ -15,18 +16,27 @@ ctypes.windll.gdi32.AddFontResourceW("assets/fonts/Montserrat/Montserrat-Light.t
 
 #Side Bar
 sidebar = ctk.CTkFrame(fenetre, width=200, corner_radius=0, fg_color="#2b2b2b")
-sidebar.pack(side="left", fill="y")
+sidebar.pack(side="left", fill="y") 
 sidebar.pack_propagate(False)
 
+#Logo
+logo = ctk.CTkImage(Image.open("assets/img/logo.png"), size=(100, 100))
+ctk.CTkLabel(sidebar, image=logo, text="Compta Clair \n Suivi de dépenses", 
+             compound="top", text_color="white").pack(pady='30')  # image en haut, texte en bas
 
+
+
+#Bouttons (ne pas oublier les commandes pour les bouttons)
 btn_dashboard = ctk.CTkButton(sidebar, text="🔰​ Tableau de bord", fg_color="gray30", hover_color="gray")
-btn_dashboard.pack()
+btn_dashboard.pack(fill='x', pady=8, padx='15')
 
 btn_dashboard = ctk.CTkButton(sidebar, text="​💲​ Dépenses", fg_color="gray30", hover_color="gray")
-btn_dashboard.pack()
+btn_dashboard.pack(fill='x', pady=8, padx='15')
 
 btn_dashboard = ctk.CTkButton(sidebar, text="📊​ Graphiques", fg_color="gray30", hover_color="gray")
-btn_dashboard.pack()
+btn_dashboard.pack(fill='x', pady=8,padx='15')
+
+# JN = ctk.CTkLabel(sidebar ,text="JN Industries \n Tous droits reservés ®️​")
 
 
 contenu = ctk.CTkFrame(fenetre, corner_radius=0, fg_color="#1f1f1f")
